9bb315234def9a34f79394df4f7f5791f1c283d7
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2019-10-15 03:21:22 +0300

    Final

--- GradientSwiftUI/ContentView.swift
+++ GradientSwiftUI/ContentView.swift
@@ -10,7 +10,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        ZStack {
+            AngularGradient(gradient: Gradient(colors: [.blue, .yellow, .green, .pink, .orange, .purple]), center: .center).edgesIgnoringSafeArea(.all)
+            RadialGradient(gradient: Gradient(colors: [.red, .yellow]), center: .center, startRadius: 20, endRadius: 250)
+            Text("Hello World").padding()
+            .foregroundColor(.white)
+            .font(.system(size: 46))
+            .background(LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .top, endPoint: .bottom))
+        }
+        
     }
 }
 
